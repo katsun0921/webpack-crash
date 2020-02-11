d636850b6119b04bf1219cdfa5ad85b0f1b5513e
katsun0921 <s.katsumasa@gmail.com>
2020-02-11 22:35:26 +0900

    Add eslint-loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import './style.scss'
-import './style.css'
-
-console.log('I am printing logs')
+import React from 'react';
+import ReactDom from 'react-dom';
+import './style.scss';
+import './style.css';
 
 ReactDom.render(
   <div>Hello, React!</div>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,29 @@
 const path = require('path');
-const outputPath = path.resolve(__dirname, "dist")
-console.log(outputPath)
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
+const outputPath = path.resolve(__dirname, 'dist');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: outputPath,
-    filename: "main.js"
+    filename: 'main.js',
   },
   module: {
     rules: [
       {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        exclude: '/node_modules/',
+        loader: 'eslint-loader',
+      },
+      {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: 'babel-loader',
       },
       {
         test: /\.(scss|css)$/,
@@ -25,33 +31,33 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           'css-loader',
           'sass-loader',
-        ]
+        ],
       },
       {
         test: /\.(jpe?g|png|gif|svg|ico)$/i,
         loader: 'url-loader',
         options: {
           limit: 2048,
-          name: './images/[name].[ext]'
-        }
+          name: './images/[name].[ext]',
+        },
       },
       {
         test: /\.html$/,
-        loader: 'html-loader'
-      }
-    ]
+        loader: 'html-loader',
+      },
+    ],
   },
   devServer: {
-    contentBase: outputPath
+    contentBase: outputPath,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      filename: './index.html'
+      filename: './index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[hash].css'
-    })
+      filename: '[name].[hash].css',
+    }),
   ],
   optimization: {
     minimizer: [
@@ -59,11 +65,12 @@ module.exports = {
         test: /\.js(\?.*)?$/i,
         uglifyOptions: {
           compress: {
-            drop_console: true
-          }
-        }
+            drop_console: true,
+          },
+        },
       }),
-      new OptimizeCSSAssetsPlugin({})
+      new OptimizeCSSAssetsPlugin({}),
     ],
   },
-}
+  devtool: 'eval-source-map',
+};
